d17ff3b16e8ac12673a8ed1e6e80f97185b5e292
unknown <darrius.clk@gmail.com>
2021-07-03 12:37:17 +0800

    random number generate

---
+++ randint.py
@@ -0,0 +1,8 @@
+print('random number generate')
+x = input('Please input the start value:')
+y = input('Please input the start value:')
+x = int(x)
+y = int(y)
+import random
+r = random.randint(x, y)
+print(r)
